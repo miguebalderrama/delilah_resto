5b0efde3eb14087e101819f76e5785f2da01cfbd
Miguel Balderrama <miguel.balderr@gmail.com>
2021-08-03 20:09:43 -0300

    vamos progresando2

--- middlewares.js
+++ middlewares.js
@@ -1,11 +1,18 @@
 const usuariosHandler = require('./usuarios_handler.js');
+const jwt = require('jsonwebtoken');
+const seed = '51mulad0res';
 
 async function validarUsuario(req, res, next) {
-    let usuariosArray = await usuariosHandler.autenticarUsuario(req.body.username, req.body.password);
-    if (usuariosArray.length === 0) {
+    let encontrado = await usuariosHandler.autenticarUsuario(req.body.username, req.body.password);
+    console.log(encontrado);
+    if (encontrado.length === 0) {
         res.status(403).send({error: 'Usuario y/o contraseña invalido'});
     }
-    
+    let token;
+    if(encontrado){
+      token=jwt.sign({usuario:encontrado.Username, rol:encontrado.Admin_rol}, seed, {expireIn:'10m'});
+    }   
+    return token; 
     next();
 }
 const isAdmin = (req, res, next) => {
